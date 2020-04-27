21535ff6913c42a5ba1aef88577ea3df9531ba9a
Mikael <lightspeed1001@gmail.com>
2020-04-27 13:49:02 +0000

    Venv now ignored. Added requirements.txt for pip.

--- captain_console/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\annag\Dropbox\HR\Tolvunarfraedi\onn_2\3week_ru\captain_console\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
