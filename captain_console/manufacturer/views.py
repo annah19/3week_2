028b83397e82e144d4da08688d7df7a6f340df21
annah19 <annah19@ru.is>
2020-05-04 11:02:20 +0000

    test2

--- captain_console/manufacturer/views.py
+++ captain_console/manufacturer/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Hello from the index view in the manufacturer app</h1>')
+
+#hæ aldfjæ
