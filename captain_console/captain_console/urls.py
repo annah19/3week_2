005c6ec79d5b99fafc0d0b523e2cd3583cf7207a
annah19 <annah19@ru.is>
2020-05-04 11:33:26 +0000

    Creating html files

--- captain_console/captain_console/urls.py
+++ captain_console/captain_console/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('CaptainConsole/', include('CaptainConsole.urls'))
+    path('CaptainConsole/', include('CaptainConsole.urls')),
+    path('manufacturers/', include('manufacturer.urls'))
 ]
