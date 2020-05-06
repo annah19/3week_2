c796e8573fc6e9d505bc2bc0517ccd0b157e11ee
Svanhvit <svanhvit19@ru.is>
2020-05-06 09:37:13 +0000

    database connections

--- captain_console/captain_console/settings.py
+++ captain_console/captain_console/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'captain_console.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bimflsqd',
+        'USER': 'bimflsqd',
+        'PASSWORD': 'ukSADTqIT-b-H63RbS7VbX9i4WkIcOkt',
+        'HOST': 'rogue.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
