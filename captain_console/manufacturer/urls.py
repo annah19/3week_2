020a4296f6493a293e3b9d5dbfd4d8a88a7fd5d0
hilarioustargaryen <dimmaogkeli@gmail.com>
2020-05-07 10:55:24 +0000

    deleting apps

--- captain_console/manufacturer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-# From candyqueen video - testing
-urlpatterns = [
-    # http://localhost:8000/manufacturers
-    path('', views.index, name="manufacturer-index"),
-]
