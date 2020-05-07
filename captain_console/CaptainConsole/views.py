dc560f62687033514bea37d72044c782939bae7e
annah19 <annah19@ru.is>
2020-05-07 12:46:15 +0000

    testing views

--- captain_console/CaptainConsole/views.py
+++ captain_console/CaptainConsole/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Testing -Not supposed to be here, check index.html also
+captainconsole = [
+    {'name':'Gameboy','price': 39.99},
+    {'name':'Gameboy','price': 49.99},
+]
 
 def index(request):
-    return render(request,'captainconsole/index.html')
+    return render(request,'captainconsole/index.html', context={'captainconsole': captainconsole})
