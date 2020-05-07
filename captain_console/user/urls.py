cdbbd96630dab7144040be644332a0644292013f
Mikael <lightspeed1001@gmail.com>
2020-05-07 21:28:34 +0000

    Added some logged in checks to the navigation bar.

--- captain_console/user/urls.py
+++ captain_console/user/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     # http://localhost:8000/user
     path('', views.profile, name="profile"),
+    path('profile', views.profile, name="profile"),
     path('wishlist', views.wishlist, name="wishlist"),
     path('register', views.register, name="register"),
     path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
