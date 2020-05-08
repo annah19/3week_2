0702a2c9c09882f4e9c047c97acaa08b9d7b8a06
annah19 <annah19@ru.is>
2020-05-08 11:26:12 +0000

    frontpage view

--- captain_console/CaptainConsole/views.py
+++ captain_console/CaptainConsole/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 
-# Testing -Not supposed to be here, check informational.html also
-captainconsole = [
-    {'name': 'Gameboy', 'price': 39.99},
-    {'name': 'Gameboy', 'price': 49.99},
+# Testing -check informational.html also
+computers = [
+    {'id': 1, 'name': 'Gameboy', 'price': 39.99},
+    {'id': 1,'name': 'Playstation1', 'price': 49.99},
+    {'id': 1,'name': 'Sega', 'price': 49.99},
+    {'id': 1,'name': 'Nes', 'price': 49.99},
+]
+games = [
+    {'id': 1,'name': 'Mario Kart', 'price': 39.99},
+    {'id': 1,'name': 'Donkey Kong', 'price': 49.99},
+    {'id': 1,'name': 'Pacman', 'price': 49.99},
+    {'id': 1,'name': 'Sonic', 'price': 49.99},
 ]
 
 
 def index(request):
-    return render(request, 'captainconsole/index.html', context={'captainconsole': captainconsole})
+    return render(request, 'captainconsole/index.html', context={'computers': computers,'games':games} )
 
 
 def faq(request):
