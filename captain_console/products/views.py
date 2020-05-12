dacc3d9f1d53df73fe2ce41c3c5c555aab734527
Mikael <lightspeed1001@gmail.com>
2020-05-12 15:01:56 +0000

    Search history saved on server.

--- captain_console/products/views.py
+++ captain_console/products/views.py
@@ -9,24 +9,22 @@ def index(request):
     search = request.GET.get("search", "")
     order_by = request.GET.get("order_by", "name"),
 
-
-
-
-    games = {}
     if search == "":
         games = Product.objects.filter(manufacturer__name__icontains=manufacturer, category__name__icontains=category)
     else:
         games = Product.objects.filter(name__icontains=search)
+        add_to_search_history(request, search)
 
     if order_by == "name" or order_by == "price":
         games = games.order_by(order_by)
-
+    search_history = get_search_history(request)
     return render(request, 'products/product_list.html', context={'products': games,
                                                                   'categories': ProductCategory.objects.all(),
                                                                   'manufacturers': ProductManufacturer.objects.all(),
                                                                   'selected_manufacturer': manufacturer,
                                                                   'selected_category': category,
-                                                                  'selected_order_by': order_by
+                                                                  'selected_order_by': order_by,
+                                                                  'search_history': search_history
                                                                   })
 
 
@@ -34,36 +32,16 @@ def get_product_by_id(request, product_id):
     context = {"product": get_object_or_404(Product, pk=product_id)}
     return render(request, "products/product_details.html", context)
 
-def add_to_search_history(request):
-    search = request.GET.get("search", "")
 
-    if search == "":
-
-        search_id = 0
-        if "search_history" in request.session:
-            if search_id in request.session["search_history"]:
-                request.session["search_history"][search_id] += 1
-            else:
-                request.session["cart"][search_id] = 1
-        else:
-            request.session["search_history"] = {}
-            request.session["cart"][search_id] = 1
+def add_to_search_history(request, search):
+    if search != "":
+        if "search_history" not in request.session:
+            request.session["search_history"] = []
+        request.session["search_history"].append(search)
         request.session.modified = True
-        return JsonResponse({"data": request.session["search_history"]})
-
-'''def get_search_history(session):
-    if "search_history" not in session:
-        return {"search_history": [], "queries": 0}
-    search_quries = session["search_history"]
-    search_history_response = []
-    queries = 0
-    for product_id in search_quries:
-        searches = search_quries[search_id]
-        id = Product.objects.get(pk=search_id)
-
-        search_dict = {}
-        search_history_response.append(search_dict)
-        queries += product_total_price
-    return {"cart": cart_response, "subtotal": cart_subtotal}'''
 
 
+def get_search_history(request):
+    if "search_history" not in request.session:
+        request.session["search_history"] = []
+    return request.session["search_history"]
